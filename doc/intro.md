b0b6aecee4871a6ce73734c090485bf997a17106
Ziracles <nigromancer1@gmail.com>
2017-04-03 12:15:18 +0000

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to nova2507
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject nova2507 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main ^:skip-aot nova2507.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
